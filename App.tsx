a38bc291b16b907f627209147c0623314c0f90c7
alyssondepaula <alyssonjuniordepaula@gmail.com>
2021-10-12 17:29:29 -0300

    :sparkles: feat: fontes roboto carregadas

--- App.tsx
+++ App.tsx
@@ -5,8 +5,30 @@ import Routes from './src/routes';
 import themes from './src/styles/themes';
 import { Provider } from 'react-redux';
 import store from './src/store';
+import { useFonts, 
+         Roboto_100Thin,
+         Roboto_300Light,
+         Roboto_400Regular,
+         Roboto_500Medium,
+         Roboto_700Bold,
+         Roboto_900Black
+       } from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+
+
+  let [fontsLoaded] = useFonts({
+    Roboto_100Thin,
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Roboto_900Black
+  });
+
+  if (!fontsLoaded){ return <AppLoading /> }
+
   return (
     
     <Provider store={store}>
