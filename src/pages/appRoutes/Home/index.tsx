1d22280cc05f6f3aa54cda8b9905512fcd975a0f
alyssondepaula <alyssonjuniordepaula@gmail.com>
2021-10-12 15:14:20 -0300

    :sparkles: feat: page home criada com apenas o header

--- src/pages/appRoutes/Home/index.tsx
+++ src/pages/appRoutes/Home/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, View } from 'react-native';
+import { useAppSelector } from '../../../hooks/useAppSelector';
+import { IUser } from '../../../store/reducers/user';
+import Header from './components/Header';
 
 import { Container, StatusBar } from './styles';
 
 const Home: React.FC = () => {
+
+
+  const [blur,setBlur] = useState<boolean>(false)
+
+  const user: IUser = useAppSelector(state=> state.user)
+
+
   return ( 
     
     <>
     <StatusBar/>
     <Container>
+    <Header name={user.data.email} isBlur={blur} setBlur={()=>setBlur(!blur)}/>
    
 
     </Container>
--- src/pages/appRoutes/Home/styles.ts
+++ src/pages/appRoutes/Home/styles.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components/native';
 
 export const StatusBar = styled.StatusBar.attrs(props=>({
-    barStyle: props.theme.isDark ? 'light-content' : 'dark-content',
+    barStyle: props.theme.isDark ? 'dark-content' : 'light-content',
     backgroundColor: props.theme.colors.primary
  }))`
  `;
 
 export const Container = styled.View`
    flex: 1;
+   background-color: ${({theme})=> theme.colors.gray};
 `;
