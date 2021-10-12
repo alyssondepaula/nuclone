28c0df0ecf577bdc9837ccc47680e9b020c0556b
alyssondepaula <alyssonjuniordepaula@gmail.com>
2021-10-12 14:50:25 -0300

    :recycle: feat: adicionado consulta ao store

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
+import { useAppSelector } from '../hooks/useAppSelector';
+import { IUser } from '../store/reducers/user';
 import AppRoutes from './appRoutes';
 import AuthRoutes from './authRoutes';
 
 
 const Routes: React.FC = () => {
 
+ 
+  const user: IUser = useAppSelector(state=> state.user)
 
-  const siggned = true;
+  const siggned = !!user.data;
 
 
   return siggned ? <AppRoutes/> : <AuthRoutes/>;
